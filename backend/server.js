33b2879b7186742c91422837e60d0501f5336657
UChariTh <109493502+UChariTh@users.noreply.github.com>
2026-04-02 07:08:36 +0530

    Add production origin to CORS configuration
    In the server.js file

--- backend/server.js
+++ backend/server.js
@@ -93,7 +93,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
     cors({
-        origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+        origin: [
+            "http://localhost:5173", 
+            "http://127.0.0.1:5173",
+            "https://e-commerce-platform-theta-silk.vercel.app" 
+        ],
         credentials: true,
     })
 );
