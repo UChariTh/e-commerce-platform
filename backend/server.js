4151271427069d8413205b4c8bc106574262d490
UChariTh <109493502+UChariTh@users.noreply.github.com>
2026-04-03 09:47:45 +0530

    Change CORS origin to allow all origins

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ const PORT = process.env.PORT || 5000;
 // }));
 
 app.use(cors({
-    origin: true, 
+    origin: "*", 
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"]
